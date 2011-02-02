e34e22d12fdd126e0479dd279839c409f4280cbd
Jonathan Zacsh <jzacsh@gmail.com>
2011-02-01 19:38:05 -0500

    Conceptual show of inheritance.

---
+++ src/in_class/02-01/Employee.java
@@ -0,0 +1,21 @@
+/**
+ * Conceptual only, don't try to compile!
+ */
+
+protected class Employee {
+    String name;
+    int id;
+
+    public Employee (String emp_name, int emp_id) {
+        this.name = emp_name;
+        this.id = emp_id;
+    }
+}
+
+public class Hourly extends Employee {
+    //stuffz
+}
+
+public class Salary extends Employee {
+    //stuffz
+}
