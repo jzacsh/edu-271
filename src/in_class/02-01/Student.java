c56dc67c9e944f953ea57add5cfd18b8674dd4e7
Jonathan Zacsh <jzacsh@gmail.com>
2011-02-01 19:32:01 -0500

    showing java allocation of memory for structure that is a class.

--- src/in_class/02-01/Student.java
+++ src/in_class/02-01/Student.java
@@ -1,5 +1,4 @@
 public class Student {
-
     private String name;
     private int id;
     public static String school = "Brookdale";
@@ -19,5 +18,15 @@ public class Student {
         System.out.println("Before creating an instance of Student class: School is: " + Student.school + "\n");
         Student sally = new Student("Sally Mae", 1234);
         System.out.println(sally); //will implictly call toString()
+
+        System.out.println("Creating enough space for 5 instantiations of the Student structure, or 'class'.");
+        //does NOT invoke Student default constructor:
+        Student students[] = new Student[5];
+        //simply creates enough null pointers of size Student
+//      System.out.println("index[1] of students stuff is: " + students[1].id);
+
+        System.out.println("Instantiating a Student object in the first allocated index[0].");
+        students[0] = new Student("Jack", 1003);
+        System.out.println("index[0] of students stuff is: " + students[0]);
     }
 }
