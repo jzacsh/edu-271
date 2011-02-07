c5e09ca4c327e7ccd135917405447be89e0157b3
Jonathan Zacsh <jzacsh@gmail.com>
2011-02-06 21:11:28 -0500

    basics of MyPoint class accessors.

--- src/due_02-08/MyPoint.java
+++ src/due_02-08/MyPoint.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
 import java.io.*;
 
+/**
+ * A simple testing class for the MyPoint class.
+ */
+public class TestMyPoint {
+    MyPoint pointy = new MyPoint();
+    double cx = pointy.getCoordinate(0);
+    double cy = pointy.getCoordinate(1);
+    System.out.printf("(x,y)\n(%d,%d)\n", cx, cy);
+}
+
+/**
+ * A class to handle (x,y) coordinate calculations.
+ */
 public class MyPoint {
     //
     //class data members
@@ -30,6 +44,49 @@ public class MyPoint {
     //
     //class methods:
     //
+
+    /**
+     * Default accessor method.
+     *
+     * @return  double[]  array of doubles representing the (x,y) coordinates
+     * of this object.
+     */
+    public double[] getCoordinate() {
+        return new double[]{ this.coordX, this.coordY};
+    }
+
+    /**
+     * Accessor method.
+     *
+     * @param  short  coordinate requested, 0 for the first, 1 for the second. (X,Y)
+     * @return  double  the x or y -coordinate requested for the object.
+     */
+    public double getCoordinate(short coord) {
+        if (coord) {
+            return this.coordY;
+        }
+        else {
+            //0 was requested
+            return this.coordX;
+        }
+    }
+
+    /**
+     * The silly way to do accessors:
+     */
+//  /**
+//   * Accessor method for X coordinate.
+//   */
+//  public double getCoordX() {
+//      return this.coordX;
+//  }
+
+//  /**
+//   * Accessor method for Y coordinate.
+//   */
+//  public double getCoordY() {
+//      return this.coordY;
+//  }
 
 
     public static void main() {
