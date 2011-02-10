6cdb42ae92346dbe43c066b8236fd456b8fee97b
Jonathan Zacsh <jzacsh@gmail.com>
2011-02-09 23:20:44 -0500

    naming the file after the test class

--- src/due_02-08/RunMyPoint.java
+++ src/due_02-08/RunMyPoint.java
@@ -5,13 +5,30 @@ import java.io.*;
 /**
  * A simple testing class for the MyPoint class.
  */
-class TestMyPoint {
+public class RunMyPoint {
     public static void main(String[] args) {
-        MyPoint pointy = new MyPoint();
+        double x = 0;
+        double y = 0;
+
+        if (args.length == 2) {
+            try {
+                x = Double.parseDouble(args[0]);
+                y = Double.parseDouble(args[1]);
+            }
+            catch (Exception e) {
+                System.err.printf("Java has no `pass` keyword?\n");
+            }
+        }
+
+        MyPoint pointy = new MyPoint(x,y);
         double cx = pointy.getCoordinate(0);
         double cy = pointy.getCoordinate(1);
-    //  System.out.println("(x,y) = ("+cx+","+cy+")");
-        System.out.printf("(x, y) = (%f, %f)\n", cx, cy);
+        System.out.printf("(x, y) = (%.2f, %.2f)\n", cx, cy);
+        double otherX = 10;
+        double otherY = 35;
+        System.out.printf("Distance between above and coords at (%.2f, %.2f) is: %.2f\n",
+                otherX, otherY, pointy.distance(otherX, otherY)
+            );
     }
 }
 
@@ -31,7 +48,7 @@ class TestMyPoint {
  * +----------------------------------------------+
  *
  */
-public class MyPoint {
+class MyPoint {
     //
     //class data members
     //
@@ -88,25 +105,6 @@ public class MyPoint {
         }
     }
 
-    //@TODO: Professor, can I remove these lines?
-
-    /**
-     * The silly way to do accessors:
-     */
-//  /**
-//   * Accessor method for X coordinate.
-//   */
-//  public double getCoordX() {
-//      return this.coordX;
-//  }
-
-//  /**
-//   * Accessor method for Y coordinate.
-//   */
-//  public double getCoordY() {
-//      return this.coordY;
-//  }
-
     /**
      * Default distance calculation between this object and another.
      *
@@ -135,31 +133,6 @@ public class MyPoint {
         return this.distance(other);
     }
 
-    //
-    //@TODO: why do my tests only run here? why not in the TestClass I created?
-    //
     public static void main(String[] args) {
-        double x = 0;
-        double y = 0;
-
-        if (args.length == 2) {
-            try {
-                x = Double.parseDouble(args[0]);
-                y = Double.parseDouble(args[1]);
-            }
-            catch (Exception e) {
-                System.err.printf("Java has no `pass` keyword?\n");
-            }
-        }
-
-        MyPoint pointy = new MyPoint(x,y);
-        double cx = pointy.getCoordinate(0);
-        double cy = pointy.getCoordinate(1);
-        System.out.printf("(x, y) = (%.2f, %.2f)\n", cx, cy);
-        double otherX = 10;
-        double otherY = 35;
-        System.out.printf("Distance between above and coords at (%.2f, %.2f) is: %.2f\n",
-                otherX, otherY, pointy.distance(otherX, otherY)
-            );
     }
 }
