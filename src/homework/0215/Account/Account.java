66d0904d145423dac893bc59a3c935882565f6cf
Jonathan Zacsh <jzacsh@gmail.com>
2011-02-15 20:08:05 -0500

    most of abstract   class Account

--- src/homework/0215/Account/Account.java
+++ src/homework/0215/Account/Account.java
@@ -25,4 +25,77 @@
  */
 
 public abstract class Account {
+    protected String id;
+    protected double balance;
+    protected double annIntRate;
+    protected java.util.Date dateCreated;
+
+    //default constructor
+    public Account() {
+        this.balance = 0;
+        this.annIntRate = 0.5;
+        this.dateCreated = new java.util.Date();
+        //making sure to have a unique id: Rather than just Math.random we're
+        //running an MD5 hash of the dateCreated and a random number.
+        String dateStamp = (String) this.dateCreated + (String) Math.random;
+        java.security.MessageDigest md5 = new java.security.MessageDigest.getInstance("MD5");
+        this.id = (String) md5.digest(dateStamp.getBytes("UTF-8"));
+    }
+
+    /**
+     * Constructor
+     *
+     * @param double id - unique identifier for this account.
+     * @param double balance - the starting balance for this account. 
+     * @param double annIntRate - the annual interest rate for this account.
+     */
+    public Account(double bal, double annIntRate) {
+        this(); //dateCreated and 'id' taken care of here.
+        this.setBalance = bal;
+        this.annIntRate = annIntRate;
+    }
+
+//@TODO: book doesn't say anything about accessor/mutator for balance?
+
+    /**
+     * Accessor for the 'balance' property
+     *
+     * @see balance.
+     * @return double the current value of the 'balance' property.
+     */
+/* remove this line
+    private double getBalance() {
+        return this.balance;
+    }
+remove this line */
+
+    /**
+     * Mutator for the 'balance' property.
+     *
+     * @see balance
+     * @param double value to set the 'balance' property to.
+     */
+/* remove this line
+    private void setBalance(double bal) {
+        this.balance = bal;
+    }
+remove this line */
+
+    /**
+     * Deposit a certain amount into the 'balance' property.
+     *
+     * @param double amount to be deposited.
+     */
+    public void deposit(double d) {
+        this.balance = this.balance + w;
+    }
+
+    /**
+     * Withdraw a certain amount from the 'balance' property.
+     *
+     * @param double amount to be withdrawn.
+     */
+    public void withdraw(double w) {
+        this.balance = this.balance - w;
+    }
 }
