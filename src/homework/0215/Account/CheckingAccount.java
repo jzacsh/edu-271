ab70860c0a1059c03dfb8f4b1510269cc2842c26
Jonathan Zacsh <jzacsh@gmail.com>
2011-02-15 20:45:11 -0500

    basic layout of CheckingAccount class

---
+++ src/homework/0215/Account/CheckingAccount.java
@@ -0,0 +1,41 @@
+public class CheckingAccount extends Account.java implements throwable {
+    protected double overDraftLimit;
+
+    //default constructor
+    public CheckingAccount() {
+        super();
+        this.setOverDraftLimit(0);
+    }
+
+    /**
+     * Constructor
+     *
+     * @see Account
+     * @see this.setOverDraftLimit
+     * @param double starting balance for this account.
+     * @param double annual interest rate for this account.
+     * @param double lower limit allowed for 'balance' property.
+     */
+    public CheckingAccount(double bal, double annIntRate, double limit) {
+        super(bal, annIntRate);
+        this.setOverDraftLimit(limit);
+    }
+
+    //override for overdrafts
+    public void withdraw(double amt) throws Error {
+        double proposedBalance = this.balance - amt;
+        if (proposedBalance < 0 && java.lang.Math.abs(proposedBalance) > this.overDraftLimit) {
+            throw Error("Over-draft Limit, %d, exceeded. Amount not withdrawn.", this.overDraftLimit);
+        }
+    }
+
+    /**
+     * Mutator for 'overDraftLimit' property.
+     *
+     * @see Account.balance
+     * @param double lower limit allowed for 'balance' property.
+     */
+    public void setOverDraftLimit(double limit) {
+        this.overDraftLimit = java.lang.Math.abs(limit);
+    }
+}
