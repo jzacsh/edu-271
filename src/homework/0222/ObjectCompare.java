17b0c0068b7539ff80d11bfcc4922129515f7226
Jonathan Zacsh <jzacsh@gmail.com>
2011-02-22 22:41:17 -0500

    started part2 of lab4 homework: Object comparisons, via array sorting.

---
+++ src/homework/0222/ObjectCompare.java
@@ -0,0 +1,10 @@
+public class ObjectCompare {
+    /**
+     * Returns the largest object in an array of objects.
+     *
+     * @param  array  a set of Objects to be searched
+     * @return  Object  the largest object in the set.
+     */
+    public static Object max(Object[] a) {
+    }
+}
