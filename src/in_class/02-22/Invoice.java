ba00be87118a948cc3b4e58ba13635e7c3c0b467
Jonathan Zacsh <jzacsh@gmail.com>
2011-02-22 21:15:39 -0500

    Example gui app

---
+++ src/in_class/02-22/Invoice.java
@@ -0,0 +1,55 @@
+import java.awt.*;
+import java.swing.*;
+
++
+/**
+ * +-------------------------------------------+
+ * | Invoice                             _ O X |
+ * |-------------------------------------------|
+ * |                                           |
+ * |  Sub Total      [________________]        |
+ * |  Discount %     [________________]        |
+ * |  Invoice Total  [________________]        |
+ * |-------------------------------------------|
+ * |      [ calculate ]        [ exit ]        |
+ * +-------------------------------------------+
+ */
+
+public class Invoice extends JFrame {
+    public Invoice() {
+        // set GridLayout: 3 rows, 2 columsns, gaps 5  between compontents
+        // horiz and vert
+        JPanel inv = new JPanel();
+        inv.setLayout(new GridLayout(3, 0, 5, 5));
+
+        //add labels and text fields to the frame
+        inv.add(new JLabel("Sub Total"));
+        inv.add(new JTextField(8));
+        inv.add(new JLabel("Discount %"));
+        inv.add(new JTextField(8));
+        inv.add(new JLabel("Invoice Total"));
+        inv.add(new JTextField(8));
+
+        //set the buttom section
+        setLayout(new BorderLayout(5, 5));
+        this.add(inv, BorderLayout.CENTER);
+        JPanel buttons = new JPanel(); //implicitly FlowLayout
+
+        //add buttons
+        JButton calc = new JButton("Calculate");
+        JButton exit = new JButton("Exit");
+        buttons.add(calc);
+        buttons.add(exit);
+        this.add(buttons, BorderLayout.SOUTH);
+    }
+
+    //main method
+    public static void main(String[] argv) {
+        Invoice frame = new Invoice();
+        frame.setTitle("Invoice");
+        frame.setTitle("");
+        frame.setTitle("");
+        frame.setTitle("");
+        this.add(new JButton(""), BorderLayout.EAST);
+    }
+}
