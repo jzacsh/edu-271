8636228dd01033c1550f01547c79ac3d0ff4f4ce
Jonathan Zacsh <jzacsh@gmail.com>
2011-02-24 18:38:40 -0500

    majority of bs for this project

--- src/homework/0222/ObjectTest.java
+++ src/homework/0222/ObjectTest.java
@@ -3,9 +3,31 @@
  */
 public class ObjecTest {
     public static void main(String[] argv) {
-        for (int i = 0; i < 4; i++) {
-            Object obj[i] = new Object();
+        int i;
+
+        //test strings
+        System.out.printf("Testing with strings\n");
+        for (i = 0; i < obs.getLength(); i++) {
+            System.out.printf("...Creating object %.0f of %.0f\n", i, obs.getLength(), ObjectCompare(obs));
+            String obs[i] = new String((String) i);
         }
-        ObjectCompare comparison = new ObjectCompare(obs);
+        System.out.printf("largest object is: %s\n", ObjectCompare(obs));
+
+        //test integers
+        System.out.printf("\nTesting with integers\n");
+        for (i = 0; i < obs.getLength(); i++) {
+            System.out.printf("...Creating object %.0f of %.0f\n", i, obs.getLength(), ObjectCompare(obs));
+            int obs[i] = new int(i);
+        }
+        System.out.printf("largest object is: %s", ObjectCompare(obs));
+
+        //test dates
+        System.out.printf("\nTesting with dates\n");
+        java.util.Date[10] obs = java.util.Date();
+        for (i = 0; i < obs.getLength(); i++) {
+            System.out.printf("...Creating object %.0f of %.0f\n", i, obs.getLength(), ObjectCompare(obs));
+            java.util.Date obs[i] = new java.util.Date();
+        }
+        System.out.printf("largest object is: %s", ObjectCompare(obs));
     }
 }
