c64724a3a23e949ebc9e26a86c2bb6b02c949a6c
Jonathan Zacsh <jzacsh@gmail.com>
2011-02-24 18:14:46 -0500

    a quick class to test ObjectCompare.java

---
+++ src/homework/0222/ObjectTest.java
@@ -0,0 +1,11 @@
+/**
+ * Test class to ObjectCompare
+ */
+public class ObjecTest {
+    public static void main(String[] argv) {
+        for (int i = 0; i < 4; i++) {
+            Object obj[i] = new Object();
+        }
+        ObjectCompare comparison = new ObjectCompare(obs);
+    }
+}
