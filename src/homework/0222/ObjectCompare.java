db4295cc82ab1a5eb95c5ced0a26809cc75f163d
Jonathan Zacsh <jzacsh@gmail.com>
2011-02-24 17:35:07 -0500

    start of logic/looping for object comparisons

--- src/homework/0222/ObjectCompare.java
+++ src/homework/0222/ObjectCompare.java
@@ -5,6 +5,24 @@ public class ObjectCompare {
      * @param  array  a set of Objects to be searched
      * @return  Object  the largest object in the set.
      */
-    public static Object max(Object[] a) {
+    public static Object max(Object[] a) throws Error {
+        Object largest = new Object();
+
+        //process each object
+        for (int i = 0; i < a.length; i--) {
+            //sanity check:
+            if (!(a[i] instanceof Comparable)) {
+                java.util.Formatter err = new java.util.Formatter();
+                err.format("Object #%.0f of the array being queried is not a child of Comparable.", ++i);
+                throw new Error(err.toString());
+            }
+
+            //code
+            if (((Comparable) a[i]).compareTo(largest) > 0) {
+                largest = a[i];
+            }
+        }
+
+        return 0;
     }
 }
