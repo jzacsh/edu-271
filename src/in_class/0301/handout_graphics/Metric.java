6e97f95609ace5149e5cc4e65a024960b012bd9d
Jonathan Zacsh <jzacsh@gmail.com>
2011-03-01 20:04:18 -0500

    just cleaned up for my own readability.

--- src/in_class/0301/handout_graphics/Metric.java
+++ src/in_class/0301/handout_graphics/Metric.java
@@ -8,8 +8,8 @@ public class Metric extends JFrame
     public Metric()
     {
         super("Font Metrics");
-        setSize (510, 610);
-        setVisible(true);
+        this.setSize (510, 610);
+        this.setVisible(true);
     }
 
     public void paint (Graphics g)
@@ -17,15 +17,15 @@ public class Metric extends JFrame
         String message = "Java is Great";
         super.paint(g); // what happens if you forget to invoke super.paint() ?
 
-        g.setFont ( new Font ("Monospaced", Font.BOLD, 20 ) );
+        g.setFont(new Font ("Monospaced", Font.BOLD, 20 ));
         FontMetrics fm = g.getFontMetrics();
 
         // find the center
         int w = fm.stringWidth(message);
         int h = fm.getAscent();
 
-        int x = (getSize().width - w) / 2;
-        int y = (getSize().height + h) / 2;
+        int x = (this.getSize().width - w) / 2;
+        int y = (this.getSize().height + h) / 2;
 
         g.drawString( message, x, y);
 
