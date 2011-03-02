43cc1c1c7781f49fa1c1656c0a653a999bf56b61
Jonathan Zacsh <jzacsh@gmail.com>
2011-03-02 00:13:52 -0500

    working version of GridLayout lab. not sure how this utilizes "panels" as the homework mentions.

--- src/homework/0301/exGridLayout.java
+++ src/homework/0301/exGridLayout.java
@@ -1,9 +1,30 @@
-public class exGridLayout {
+import javax.swing.*;
+import java.awt.*;
+
+public class exGridLayout extends JFrame {
     //default constructor
     public exGridLayout() {
+        this.setLayout(new GridLayout(2, 3));
+        //panel top
+        this.add(new JButton("Button 1"));
+        this.add(new JButton("Button 2"));
+        this.add(new JButton("Button 3"));
+
+        //panel bottom
+        this.add(new JButton("Button 4"));
+        this.add(new JButton("Button 5"));
+        this.add(new JButton("Button 6"));
     }
 
     //main
     public static void main(String[] argv) {
+        //create frame
+        exGridLayout fr = new exGridLayout();
+
+        fr.setTitle("Example GridLayout, Assignment 12.3");
+        fr.setLocationRelativeTo(null);
+        fr.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        fr.setSize(400,350);
+        fr.setVisible(true);
     }
 }
