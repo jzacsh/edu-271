41abdfd7486e76fe53f60e917877b4aa0aa906ba
Jonathan Zacsh <jzacsh@gmail.com>
2011-03-02 23:34:18 -0500

    utilizing panels *and* sticking strictly to the GridLayout.

--- src/homework/0301/exGridLayout.java
+++ src/homework/0301/exGridLayout.java
@@ -4,16 +4,16 @@ import java.awt.*;
 public class exGridLayout extends JFrame {
     //default constructor
     public exGridLayout() {
-        this.setLayout(new GridLayout(1, 2));
+        this.setLayout(new GridLayout(1, 2, 10, 0));
         //panel top
-        JPanel leftPanel = new JPanel(new FlowLayout(FlowLayout.CENTER, 10, 10));
+        JPanel leftPanel = new JPanel(new GridLayout(1, 3, 5, 0));
         leftPanel.add(new JButton("Button 1"));
         leftPanel.add(new JButton("Button 2"));
         leftPanel.add(new JButton("Button 3"));
         this.add(leftPanel);
 
         //panel bottom
-        JPanel rightPanel = new JPanel(new FlowLayout(FlowLayout.CENTER, 10, 10));
+        JPanel rightPanel = new JPanel(new GridLayout(1, 3, 5, 0));
         rightPanel.add(new JButton("Button 4"));
         rightPanel.add(new JButton("Button 5"));
         rightPanel.add(new JButton("Button 6"));
@@ -28,7 +28,7 @@ public class exGridLayout extends JFrame {
         fr.setTitle("Example GridLayout, Assignment 12.3");
         fr.setLocationRelativeTo(null);
         fr.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        fr.setSize(700,100);
+        fr.setSize(600,60);
         fr.setVisible(true);
     }
 }
