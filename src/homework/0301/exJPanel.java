b1673b81223d6e534a7c6b60f35b889d594f4832
Jonathan Zacsh <jzacsh@gmail.com>
2011-03-06 17:11:51 -0500

    start of final part for lab #5

---
+++ src/homework/0301/exJPanel.java
@@ -0,0 +1,20 @@
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * 12.4
+ * (Using JPanel to group buttons) Rewrite the preceding program to create the
+ * same user interface. Instead of creating buttons and panels separately, define
+ * a class that extends the JPanel class. Place three buttons in your panel
+ * class, and create two panels from the user-defined panel class.
+ */
+
+public class exJPanel extends JPanel {
+    //default constructor
+    public exJPanel() {
+    }
+
+    //main
+    public static void main(String[] argv) {
+    }
+}
