44e9be0dcc2b89258e0808672046645b56c14117
Jonathan Zacsh <jzacsh@gmail.com>
2011-03-08 20:49:36 -0500

    added logic for speed change with every bounce.

--- src/in_class/0308/handouts/AnimationDemo.java
+++ src/in_class/0308/handouts/AnimationDemo.java
@@ -21,6 +21,8 @@ public class AnimationDemo extends JFrame {
   // Displaying a moving message
   static class MovingMessagePanel extends JPanel {
     private String message = "Welcome to Java";
+    private static int changeSpeedX = 0;
+    private static int changeSpeedY = 0;
     private static int tempXMove = 0;
     private static int tempYMove = 0;
     private static int xmovement = 10;
@@ -38,36 +40,60 @@ public class AnimationDemo extends JFrame {
       timer.start();
     }
 
+
     /** Paint message */
     public void paintComponent(Graphics g) {
       super.paintComponent(g);
 
+      //gradually change speeds
+      System.out.printf("speed =  x:%s; y:%s)\n", xmovement, ymovement);
+      if (changeSpeedX != Xdirection) {
+          changeSpeedX = Xdirection;
+          xmovement = (int) ( java.lang.Math.random() * 10 ) + 1;
+          ymovement = (int) ( java.lang.Math.random() * 10 ) + 1;
+      }
+      if (changeSpeedY != Ydirection) {
+          changeSpeedY = Ydirection;
+          xmovement = (int) ( java.lang.Math.random() * 10 ) + 1;
+          ymovement = (int) ( java.lang.Math.random() * 10 ) + 1;
+      }
 
-      System.out.printf("\n\nstarting (x, y) = (%s, %s)\n", xCoordinate, yCoordinate);
+
+      System.out.printf("\n\nstarting (x, y) = (%s, %s) @ %s/%s\n",
+              xCoordinate,
+              yCoordinate,
+              xmovement,
+              ymovement
+              );
       if (yCoordinate > getHeight()) {
-          System.out.printf("past TOP (y-coord = %s; height = %s)\n", xCoordinate, getHeight());
+          System.out.printf("hit TOP (y-coord = %s; height = %s)\n", xCoordinate, getHeight());
           Ydirection = -1;
       }
       else if (yCoordinate < 0) {
           //@TODO: this doesn't make sense
-          System.out.printf("past BOTTOM (y-coord = %s; height = %s)\n", yCoordinate, getHeight());
+          System.out.printf("hit BOTTOM (y-coord = %s; height = %s)\n", yCoordinate, getHeight());
           Ydirection = 1;
       }
 
       System.out.printf("starting xCoordinate: %s\n", xCoordinate);
       if (xCoordinate > getWidth()) {
-          System.out.printf("past RIGHT (x-coord = %s; width = %s)\n", xCoordinate, getWidth());
+          System.out.printf("hit RIGHT (x-coord = %s; width = %s)\n", xCoordinate, getWidth());
           Xdirection = -1;
       }
       else if (xCoordinate < 0) {
           //@TODO: this doesn't make sense
-          System.out.printf("past LEFT (x-coord = %s; width = %s)\n", xCoordinate, getWidth());
+          System.out.printf("hit LEFT (x-coord = %s; width = %s)\n", xCoordinate, getWidth());
           Xdirection = 1;
       }
 
       xCoordinate += Xdirection *  xmovement;
       yCoordinate += Ydirection *  ymovement;
-      System.out.printf("new (x, y) = (%s, %s)\n\n", xCoordinate, yCoordinate);
+      System.out.printf("new (x, y) = (%s, %s) @ %s/%s\n\n",
+              xCoordinate,
+              yCoordinate,
+              xmovement,
+              ymovement
+              );
 
       g.drawString(message, xCoordinate, yCoordinate);
     }
