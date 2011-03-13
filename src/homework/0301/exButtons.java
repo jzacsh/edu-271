216b470f45874bd3ddcb22eb7f7f9966f19cb8bc
Jonathan Zacsh <jzacsh@gmail.com>
2011-03-13 18:36:14 -0400

    started  end of panel homeworks.

--- src/homework/0301/exButtons.java
+++ src/homework/0301/exButtons.java
@@ -9,9 +9,27 @@ import java.awt.*;
  * class, and create two panels from the user-defined panel class.
  */
 
-public class exJPanel extends JPanel {
+public class exButtons extends JFrame {
+    //default constructor.
+    public exButtons {
+        super(); //usually implicitly called -- this created the mane frame
+        exJPanel left  = new exJPanel();
+        exJPanel right = new exJPanel();
+        left.setLayout(new FlowLayout(FlowLayout.CENTER, 20, 10));
+        right.setLayout(new FlowLayout(FlowLayout.CENTER, 20, 10));
+
+        this.setLayout(new FlowLayout(FlowLayout.CENTER, 20, 10));
+    }
+}
+
+class exJPanel extends JPanel {
     //default constructor
     public exJPanel() {
+        this.super(); //implied
+
+        this.addButton("Button a");
+        this.addButton("Button b");
+        this.addButton("Button c");
     }
 
     //main
