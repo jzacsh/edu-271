5dcd6f1175b3648d420fc60d582a16482ff74291
Jonathan Zacsh <jzacsh@gmail.com>
2011-03-14 20:35:59 -0400

    starting lab #7

---
+++ src/homework/0315/distConverter.java
@@ -0,0 +1,27 @@
+import javax.swing.*;
+import java.awt.event.*;
+import java.awt.Panel;
+import java.awt.GridLayout;
+import java.awt.BorderLayout;
+
+/**
+ *  Jonathan Zacsh
+ *
+ *  Lab 7; Exercise 15.5
+ *  pg 527; #15.5
+ */
+
+public class distConverter extends JFrame implements ActionListener {
+    //default constrcutor
+    public distConverter() {
+    }
+
+    //interface for event listener
+    public void actionPerformed(ActionEvent e) {
+    }
+
+    //main
+    public static void main(String[] argv) {
+        //
+    }
+}
