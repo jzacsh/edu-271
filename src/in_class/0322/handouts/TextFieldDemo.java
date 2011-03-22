4d0bdebdcbb611e947f8a2daab283e6d5e452518
Jonathan Zacsh <jzacsh@gmail.com>
2011-03-22 19:55:56 -0400

    explicit use of `this` object.

--- src/in_class/0322/handouts/TextFieldDemo.java
+++ src/in_class/0322/handouts/TextFieldDemo.java
@@ -19,10 +19,9 @@ public class TextFieldDemo extends RadioButtonDemo {
     // Create a new panel to hold label and text field
     JPanel jpTextField = new JPanel();
     jpTextField.setLayout(new BorderLayout(5, 0));
-    jpTextField.add(
-      new JLabel("Enter a new message"), BorderLayout.WEST);
+    jpTextField.add(new JLabel("Enter a new message"), BorderLayout.WEST);
     jpTextField.add(jtfMessage, BorderLayout.CENTER);
-    add(jpTextField, BorderLayout.NORTH);
+    this.add(jpTextField, BorderLayout.NORTH);
 
     jtfMessage.setHorizontalAlignment(JTextField.RIGHT);
 
