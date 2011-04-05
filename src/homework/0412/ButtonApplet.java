b1d217c9d867e45a8dde901d8f15b56bab0daf7e
Jonathan Zacsh <jzacsh@gmail.com>
2011-04-05 08:14:13 -0400

    lab #9; starting an applet-version of ButtonDemo standalone app.

---
+++ src/homework/0412/ButtonApplet.java
@@ -0,0 +1,68 @@
+import java.awt.*;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.*;
+
+/**
+ * This file is "Listing 15.2" out of ISBN: 0-13-222158-6
+ */
+public class ButtonApplet extends JApplet {
+    //Create a panel for displaying message
+    protected MessagePanel messagePanel = new MessagePanel("Welcome ot Java");
+
+    //declare two buttons to move the message left and right
+    private JButton jbtLeft = new JButton("<=");
+    private JButton jbtRight = new JButton("=>");
+
+    public static void init(String[] args) {
+        ButtonApplet frame = new ButtonApplet();
+
+        frame.setTitle("ButtonApplet");
+        frame.setLocationRelativeTo(null); //center the frame
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(250, 100);
+        frame.setVisible(true);
+    }
+
+    public void init {
+        //set the background color of messagePanel
+        messagePanel.setBackground(Color.white);
+
+        //create a panel jpButtons to hold two Buttons (right and left)
+        JPanel jpButtons = new JPanel();
+        jpButtons.setLayout(new FlowLayout());
+        jpButtons.add(jbtLeft);
+        jpButtons.add(jbtRight);
+
+        //set keyboard mnemonics
+        jbtLeft.setMnemonic('L');
+        jbtRight.setMnemonic('R');
+
+        //set icons and remove text
+        //jbtLeft.setIcon(new ImageIcon("image/left.gif"));
+        //jbtRight.setIcon(new ImageIcon("image/right.gif"));
+        //jbtLeft.setText(null);
+        //jbtRight.setText(null);
+
+        //set tool tip text on buttons
+        jbtLeft.setToolTipText("Move message to the Left");
+        jbtRight.setToolTipText("Move message to the Right");
+
+        //place panels in the frame
+        this.setLayout(new BorderLayout());
+        this.add(messagePanel, BorderLayout.CENTER);
+        this.add(jpButtons, BorderLayout.SOUTH);
+
+        //register anonymous listeners with the buttons
+        jbtLeft.addActionListener(new ActionListener{
+            public void actionPerformed(ActionEvent e) {
+                messagePanel.moveLeft();
+            }
+        });
+        jbtRight.addActionListener(new ActionListener{
+            public void actionPerformed(ActionEvent e) {
+                messagePanel.moveRight();
+            }
+        });
+    }
+}
