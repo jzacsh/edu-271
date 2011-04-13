85514b69e2afb922b5838dc0226b9e33e32c2913
Jonathan Zacsh <jzacsh@gmail.com>
2011-04-12 21:17:56 -0400

    fixed exam problem #23

--- src/exam/examTwo.java
+++ src/exam/examTwo.java
@@ -3,25 +3,25 @@ import javax.swing.*;
 
 public class examTwo extends JFrame {
     public examTwo() {
-        this.setLayout(new GridLayout(4, 1, 0, 3));
+        this.setLayout(new BorderLayout(5, 5));
 
         //create panel
-        JPanel Coins = new JPanel();
+        JPanel Coins = new JPanel(new GridLayout(4, 1, 0, 3));
 
         Coins.add(new JButton("Nickel"));
         Coins.add(new JButton("Dime"));
         Coins.add(new JButton("Quarter"));
         Coins.add(new JButton("Return"));
 
-        this.add(Coins);
+        this.add(Coins, BorderLayout.CENTER);
     }
 
     public static void main(String[] args) {
-        JFrame frame = new JFrame();
+        examTwo frame = new examTwo();
 
         frame.setTitle("Exam 2 - Problem #23");
         frame.setLocationRelativeTo(null);
-        frame.setSize(300, 1200);
+        frame.setSize(300, 800);
         frame.setVisible(true);
     }
 }
