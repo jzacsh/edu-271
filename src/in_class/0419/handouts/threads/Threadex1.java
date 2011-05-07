f05236abcc7bee2f156f943cd1a32b239be35ab8
Jonathan Zacsh <jzacsh@gmail.com>
2011-05-07 19:59:13 -0400

    code style cleanup

--- src/in_class/0419/handouts/threads/Threadex1.java
+++ src/in_class/0419/handouts/threads/Threadex1.java
@@ -3,22 +3,19 @@ import java.util.*;
 import java.io.*;
 
 
-public class Threadex1 
-{
-    public static void main(String[] args) 
-    {
+public class Threadex1 {
+    public static void main(String[] args) {
         Thread count1 = new CountDownEven();
         Thread count2 = new CountDownOdd();
         count1.start();
         count2.start();
         System.out.println("Running ..... ");
-        
+
     }
 }
-class CountDownEven extends Thread
-{
-    public void run()
-    {
+
+class CountDownEven extends Thread {
+    public void run() {
         int k;
         for (k = 10; k > 0 ; k -= 2 )
         {
@@ -27,10 +24,9 @@ class CountDownEven extends Thread
         }
     }
 }
-class CountDownOdd extends Thread
-{
-    public void run()
-    {
+
+class CountDownOdd extends Thread {
+    public void run() {
         int k;
         for (k = 9; k > 0 ; k -= 2 )
         {
